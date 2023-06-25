54739e9513850170d3e1472714f86b3d0ef5928c
Fritz 179 <fabrizio.cortesi179@gmail.com>
2023-06-25 22:19:58 +0200

    Use ndarray + save videos + varibale batch size

--- src/main.rs
+++ src/main.rs
@@ -17,14 +17,93 @@ fn tekenen_to_sample(image: &Tekenen, value: f32) -> Samples {
 }
 
 mod nn;
-use std::time::Instant;
+use std::{time::Instant, process::Command, fmt::format};
 
 use nn::{NN, Sample, Samples};
 
-use rand::seq::SliceRandom;
+use rand::{seq::SliceRandom, Rng};
 use tekenen::{Platform, PlatformTrait, IntervalDecision, Event, Tekenen, colors, ui::*};
 
 use crate::preloaded::load_preloaded;
+
+use image;
+
+static mut id: i32 = 0;
+
+fn save_frame(nn: &mut Box<NN>, at: f32, path: &str) {
+    let scale = 20;
+    let size = 28 * scale;
+    
+    let mut tekenen = Tekenen::new(size, size);
+
+    for x in 0..size {
+        for y in 0..size {
+            let mut c = nn.get(&vec![x as f32 / size as f32, y as f32 / size as f32, at])[0];
+
+            if c < 0.0 { c = 0.0 }
+            if c > 1.0 { c = 1.0 }
+
+            let c = (c * 255.0) as u8;
+
+            tekenen.set_pixel(x as i32, y as i32, [c, c, c, 255]);
+        }
+    }
+
+    let buffer: &[u8] = tekenen.get_pixels();   
+
+    let path = format!("./saves/{path}");
+
+    println!("Saving image at: {path}");
+    let path = std::path::Path::new(&path);
+
+    image::save_buffer(&path, buffer, size as u32, size as u32, image::ColorType::Rgba8).unwrap();
+}
+
+fn save(nn: &mut Box<NN>, at: f32, num: f32) {
+    let mut rng = rand::thread_rng();
+
+    let att = format!("{num}").replace(".", "_");
+
+    let this_id = unsafe {
+        id += 1;
+        id - 1
+    };
+
+    let path = format!("nn_image_at_{att}_{}_{}.png", this_id, rng.gen_range(0..999));
+
+    save_frame(nn, at, &path);
+}
+
+fn save_video(nn: &mut Box<NN>) {
+    let this_id = unsafe {
+        id += 1;
+        id - 1
+    };
+
+    let images = 500;
+
+    let start = -4.5;
+    let end = 4.5;
+
+    let step = (end - start) / images as f32;
+
+    for i in 0..images {
+        let at = start + i as f32 * step;
+
+        save_frame(nn, at, &format!("vid_{this_id}_{i}.png"))
+    }
+
+    Command::new("ffmpeg")
+            .args([
+                "-framerate", "5", 
+                "-i", &format!("'./saves/vid_{this_id}_%d.png'"), 
+                &format!("./saves/vid_{this_id}.mp4")
+                ])
+            .output()
+            .unwrap();
+
+    // ffmpeg -framerate 25 -i './saves/vid_0_%d.png' ./vid_a.mp4
+}
 
 fn main() {
     let and_data = vec![
@@ -48,7 +127,7 @@ fn main() {
         Sample { input: vec![1.0, 1.0], output: vec![0.0] },
     ];
 
-    let arch = [3, 18, 13, 9, 1];
+    let arch = [3, 27, 27, 9, 1];
     let mut nn = NN::new(&arch);
 
     let preloaded = load_preloaded();
@@ -69,11 +148,18 @@ fn main() {
     let mut rate = 0.1;
     let mut graph = vec![];
 
-    let mut scroller = widgets::Scroller::new(50, 250, 500);
-    scroller.min = -5.0;
-    scroller.max = 5.0;
+    let mut rate_slider = widgets::Slider::new(50, 250, 500);
+    rate_slider.min = -5.0;
+    rate_slider.max = 5.0;
+
+    let mut batch_slider = widgets::Slider::new(50, 250, 550);
+    batch_slider.min = 0.0;
+    batch_slider.max = 200.0;
+    batch_slider.value = 20.0;
 
     let mut start = Instant::now();
+
+    let mut active = true;
 
     Platform::set_interval(move || {
 
@@ -88,18 +174,30 @@ fn main() {
                         'i' => rate *= 2.0,
                         'o' => rate /= 2.0,
                         'p' => println!("{:?}", nn),
+                        ' ' => active = !active,
+                        'r' => {
+                            start = Instant::now();
+                            nn = NN::new(&arch);
+                            graph = Vec::new();
+                            training_iterations = 0;
+                        },
+                        's' => save(&mut nn, rate_slider.value, rate_slider.value + 4.5),
+                        'v' => save_video(&mut nn),
                         _ => { }
                     }
                     println!("{char}")
                 },
                 Event::MouseDown { x, y } => {
-                    scroller.mouse_down(x, y);
+                    rate_slider.mouse_down(x, y);
+                    batch_slider.mouse_down(x, y);
                 },
                 Event::MouseMove { x, y } => {
-                    scroller.mouse_move(x, y);
+                    rate_slider.mouse_move(x, y);
+                    batch_slider.mouse_move(x, y);
                 },
                 Event::MouseUp { x, y } => {
-                    scroller.mouse_up(x, y);
+                    rate_slider.mouse_up(x, y);
+                    batch_slider.mouse_up(x, y);
                 },
                 _ => { }
             }
@@ -109,22 +207,26 @@ fn main() {
         tekenen.background(colors::GRAY);
 
         let score = nn.score(&training_data);
-        graph.push(score);
+
+        if active {
+            graph.push(score);
+        }
 
         let running = Instant::now() - start;
 
-        tekenen.draw_text(&format!("Score: {}", score), 450, 450);
+        tekenen.draw_text(&format!("Score: {}", score), 450, 425);
+        tekenen.draw_text(&format!("Batch size: {}", batch_slider.value as usize), 450, 450);
         tekenen.draw_text(&format!("Iteration: {}", training_iterations), 450, 475);
         tekenen.draw_text(&format!("Rate: {rate}"), 450, 500);
         tekenen.draw_text(&format!("Elapsed: {}", running.as_secs()), 450, 525);
-        tekenen.draw_text(&format!("Slider: {}", scroller.value), 450, 550);
+        tekenen.draw_text(&format!("Drawing: {}", rate_slider.value), 450, 550);
         tekenen.draw_text(&format!("Arch: {:?}", arch), 450, 575);
 
 
         let mut rng = rand::thread_rng();
 
-        let batch_size = 10;
-        while !Platform::get_remaining_time().is_zero() {
+        let batch_size = batch_slider.value as usize;
+        while !Platform::get_remaining_time().is_zero() && active {
             training_data.shuffle(&mut rng);
 
             for i in 0..(training_data.len() / batch_size) - 1 {
@@ -135,6 +237,10 @@ fn main() {
             }
 
             training_iterations += 1;
+
+            if training_iterations % 100 == 0 {
+                println!("{}", running.as_millis())
+            }
         }
 
         // draw original image
@@ -147,7 +253,7 @@ fn main() {
         let size = 28 * scale;
         for x in 0..size {
             for y in 0..size {
-                let mut c = nn.get(&vec![x as f32 / size as f32, y as f32 / size as f32, scroller.value])[0];
+                let mut c = nn.get(&vec![x as f32 / size as f32, y as f32 / size as f32, rate_slider.value])[0];
 
                 if c < 0.0 { c = 0.0 }
                 if c > 1.0 { c = 1.0 }
@@ -160,9 +266,10 @@ fn main() {
 
         for layer_i in 0..nn.layers.len() {
             let layer = &nn.layers[layer_i];
+            let layer_bias_b = layer.bias_b.borrow();
 
             for neuron_i in 0..layer.len() {
-                let mut c = layer[neuron_i].bias_b;
+                let mut c = layer_bias_b[neuron_i];
                 let mut b = 0;
                 if c < -1.0 { c = -1.0; b = 100 }
                 if c > 1.0 { c = 1.0; b = 100 }
@@ -180,7 +287,7 @@ fn main() {
 
             for curr_i in 0..curr_layer.len() {
                 for prev_i in 0..prev_layer.len() {
-                    let mut c = connection[curr_i][prev_i].value_w;
+                    let mut c = connection.value_w[(curr_i, prev_i)];
                     let mut b = 0;
                     if c < -1.0 { c = -1.0; b = 100 }
                     if c > 1.0 { c = 1.0; b = 100 }
@@ -199,7 +306,8 @@ fn main() {
         }
 
         // Draw scroller
-        scroller.display(&mut tekenen);
+        rate_slider.display(&mut tekenen);
+        batch_slider.display(&mut tekenen);
 
         // draw graph
         let x1 = 450;
@@ -228,3 +336,167 @@ fn main() {
         IntervalDecision::Repeat
     }, 10)
 }
+
+/*
+
+// Before
+
+3245
+6542
+10030
+13232
+16343
+19527
+22638
+26033
+29137
+32536
+
+3236
+6231
+9426
+12429
+15540
+18637
+21636
+24735
+27934
+31129
+
+// No clicks
+3236
+6231
+9426
+12429
+15540
+18637
+21636
+24735
+27934
+31129
+
+// Changed layer indexing
+3730
+7332
+10938
+14537
+18240
+22339
+26854
+30950
+34539
+38244
+
+3643
+7234
+10750
+14446
+18139
+21833
+25433
+29141
+32746
+36628
+
+3929
+7540
+11140
+14629
+18245
+21835
+25539
+29130
+32851
+36645
+
+// Changed Matrix indexing
+3336
+6644
+9944
+13237
+16538
+20037
+23442
+26846
+30251
+33638
+
+3326
+6642
+9941
+13638
+17236
+20530
+23936
+27343
+30737
+34142
+
+3633
+6939
+10241
+13641
+16948
+20235
+23633
+26947
+30350
+33946
+
+// Using ndarray
+4741
+9342
+13951
+18557
+23163
+27765
+32339
+36956
+41641
+46964
+
+4653
+9255
+13863
+18539
+23162
+27860
+32541
+37257
+42038
+47042
+
+4839
+9452
+14053
+18739
+23356
+28163
+32862
+37837
+42563
+47238
+
+// matrix multiplication
+3334
+6434
+9625
+12827
+15938
+19227
+22936
+26647
+29934
+33238
+
+3931
+7643
+11342
+16551
+20446
+24337
+28352
+32239
+35942
+39531
+ */
--- src/nn.rs
+++ src/nn.rs
@@ -1,21 +1,23 @@
+use std::{cell::RefCell, borrow::BorrowMut, ops::{Mul, AddAssign}};
+
 use rand::Rng;
+use ndarray::{arr2, arr1, Array1, Array2, Array, Axis};
 
-#[derive(Debug)]
-pub struct Neuron {
-    value_a: f32,
-    unscaled_z: f32,
-    error_z: f32,
-    pub bias_b: f32,
-    gradient_b: f32
+#[derive(Debug, Default)]
+pub struct Layer {
+    value_a: RefCell<Array1<f32>>,
+    unscaled_z: RefCell<Array1<f32>>,
+    error_z: RefCell<Array1<f32>>,
+    pub bias_b: RefCell<Array1<f32>>,
+    gradient_b: RefCell<Array1<f32>>
 }
 
-#[derive(Debug)]
-pub struct Connection {
-    pub value_w: f32,
-    gradient_w: f32,
+impl Layer {
+    pub fn len(&self) -> usize {
+        self.value_a.borrow().len()
+    }
 }
 
-pub type Layer = Vec<Neuron>;
 pub type Input = Vec<f32>;
 pub type Output = Vec<f32>;
 pub struct Sample {
@@ -25,46 +27,62 @@ pub struct Sample {
 
 pub type Samples = Vec<Sample>;
 
+fn new_vec(size: usize) -> Array1<f32> {
+    let mut vec = Vec::with_capacity(size);
+
+    for _ in 0..size  {
+        vec.push(0.0);
+    }
+
+    arr1(&vec)
+}
+
 
 fn new_layer(size: usize) -> Layer {
     let mut rng = rand::thread_rng();
-    let mut neurons = Vec::with_capacity(size);
+    
+    let layer = Layer {
+        value_a: RefCell::new(new_vec(size)),
+        unscaled_z: RefCell::new(new_vec(size)),
+        error_z: RefCell::new(new_vec(size)),
+        bias_b: RefCell::new(new_vec(size)),
+        gradient_b: RefCell::new(new_vec(size)),
+    };
 
     // Init each neuron with a random bias
-    for _ in 0..size as usize {
-        neurons.push(Neuron {
-            value_a: 0.0,
-            unscaled_z: 0.0,
-            error_z: 0.0,
-            bias_b: rng.gen_range(-1.0..1.0),
-            gradient_b: 0.0
-        })
+    let mut bias = layer.bias_b.borrow_mut();
+    for i in 0..size  {
+        bias[i] = rng.gen_range(-1.0..1.0);
     }
 
-    neurons
+    drop(bias);
+
+    layer
 }
 
-type Connections = Vec<Vec<Connection>>;
+#[derive(Debug)]
+pub struct Connections {
+    pub value_w: Array2<f32>,
+    gradient_w: Array2<f32>,
+}
 
 fn new_connections(current: &Layer, previous: &Layer) -> Connections {
     let mut rng = rand::thread_rng();
-    let mut connections = Vec::with_capacity(current.len());
+
+    let mut value = Array::<f32, _>::zeros((current.len(), previous.len()));
+    let gradient = Array::<f32, _>::zeros((current.len(), previous.len()));
 
     // Init each connection with a random weight
-    for _ in 0..current.len() {
-        let mut current_connections = Vec::with_capacity(previous.len());
-
-        for _ in 0..previous.len() {                
-            current_connections.push(Connection {
-                value_w: rng.gen_range(-1.0..1.0),
-                gradient_w: 0.0,
-            })
+    for i in 0..current.len() {
+        for j in 0..previous.len() {   
+            value[(i, j)] = rng.gen_range(-1.0..1.0);                
         }
-
-        connections.push(current_connections)
     }
 
-    connections
+    Connections {
+        value_w: value,
+        gradient_w: gradient
+    }
 }
 
 #[derive(Debug)]
@@ -102,40 +120,88 @@ impl NN {
     }
 
     // let out = io[0] as f32 * nn.wa + io[1] as f32 * nn.wb + nn.b;
+    // fn forward_old(&mut self, input: &Input) {
+
+    //     let layer0 = self.layers[0].borrow_mut();
+
+    //     // layer 0 is input
+    //     assert_eq!(input.len(), layer0.len(), "Input layers not of same size!");
+
+    //     // could probably swap layer 1 for input layer?
+    //     let mut value_a = layer0.value_a.borrow_mut();
+    //     for i in 0..input.len() {
+    //         value_a[i] = input[i]
+    //     }
+
+    //     drop(value_a);
+
+    //     // calculate value for each successive layer
+    //     for curr_layer_i in 1..self.layers.len() {
+
+    //         let connection = &self.connections[curr_layer_i - 1];
+
+    //         let mut curr_unscaled_z = self.layers[curr_layer_i].unscaled_z.borrow_mut();
+    //         let mut curr_value_a = self.layers[curr_layer_i].value_a.borrow_mut();
+
+
+    //         let prev_bias_b = self.layers[curr_layer_i - 1].bias_b.borrow();
+    //         let prev_value_a = self.layers[curr_layer_i - 1].value_a.borrow();
+
+    //         for curr_node_i in 0..curr_value_a.len() {
+
+    //             // add the bias
+    //             let mut unscaled_z = prev_bias_b[curr_node_i];
+        
+    //             // add all weights
+    //             for prev_node_i in 0..prev_value_a.len() {
+    //                 unscaled_z += prev_value_a[prev_node_i] * connection.value_w[(curr_node_i, prev_node_i)];
+    //             }
+        
+    //             // activation function
+    //             curr_unscaled_z[curr_node_i] = unscaled_z;
+    //             curr_value_a[curr_node_i] = activate_sigmoid(unscaled_z);
+    //         }
+    //     }
+    // }
+
     fn forward(&mut self, input: &Input) {
 
+        let mut layer0_value_a = self.layers[0].value_a.borrow_mut();
+
         // layer 0 is input
-        assert_eq!(input.len(), self.layers[0].len(), "Input layers not of same size!");
+        assert_eq!(input.len(), layer0_value_a.len(), "Input layers not of same size!");
 
         // could probably swap layer 1 for input layer?
         for i in 0..input.len() {
-            self.layers[0][i].value_a = input[i]
+            layer0_value_a[i] = input[i]
         }
+
+        drop(layer0_value_a);
 
         // calculate value for each successive layer
         for curr_layer_i in 1..self.layers.len() {
 
             let connection = &self.connections[curr_layer_i - 1];
 
-            let layers_len = self.layers[curr_layer_i].len();
-            for curr_node_i in 0..layers_len {
-                let prev_layer = &self.layers[curr_layer_i - 1];
+            let curr_layer = &self.layers[curr_layer_i];
+            let prev_layer = &self.layers[curr_layer_i - 1];
 
-                let curr_connection = &connection[curr_node_i];
-                let curr_node = &self.layers[curr_layer_i][curr_node_i];
+            let prev_value_a = prev_layer.value_a.borrow();
+            let curr_bias_b = curr_layer.bias_b.borrow();
 
-                // add the bias
-                let mut unscaled_z = curr_node.bias_b;
-        
-                // add all weights
-                for prev_node_i in 0..prev_layer.len() {
-                    unscaled_z += prev_layer[prev_node_i].value_a * curr_connection[prev_node_i].value_w;
-                }
-        
-                // activation function
-                self.layers[curr_layer_i][curr_node_i].unscaled_z = unscaled_z;
-                self.layers[curr_layer_i][curr_node_i].value_a = activate_sigmoid(unscaled_z);
-            }
+            let mut curr_unscaled_z = curr_layer.unscaled_z.borrow_mut();
+            // TODO: assign instead of add
+
+            *curr_unscaled_z = prev_value_a.dot(&connection.value_w.t());
+            *curr_unscaled_z += &*curr_bias_b;
+
+            let activated = curr_unscaled_z.map(|unscaled| -> f32 {
+                activation_function(*unscaled)
+            });
+
+            let mut curr_value_a = curr_layer.value_a.borrow_mut();
+            *curr_value_a = activated;
+            //self.layers[curr_layer_i].unscaled_z.assign(&(self.layers[curr_layer_i].bias_b.add(&self.layers[curr_layer_i - 1].value_a.dot(&connection.value_w))))
         }
     }
 
@@ -146,61 +212,130 @@ impl NN {
 
         let mut out = Vec::with_capacity(layer.len());
 
-        for value in layer.iter() {
-            out.push(value.value_a)
+        for value in layer.value_a.borrow().iter() {
+            out.push(*value)
         };
 
         out
     }
 
+    // fn backpropagete_old(&mut self, output: &Output) {
+    //     let layers_len = self.layers.len();
+
+    //     {
+    //         // set last layer error
+    //         let last_layer = &mut self.layers[layers_len - 1].borrow_mut();
+
+    //         for i in 0..last_layer.len() {
+    //             // TODO: Why nuron.value_a - output[i] and not vice versa?
+    //             last_layer.error_z[i] =  (last_layer.value_a[i] - output[i]) * activate_sigmoid_derivate(last_layer.unscaled_z[i])
+    //         }
+    //     }
+
+
+    //     // loop for each previous layer
+    //     for curr_layer_i in (1..layers_len).rev() {
+    //         let mut curr_layer = self.layers[curr_layer_i].borrow_mut();
+    //         let mut prev_layer = self.layers[curr_layer_i - 1].borrow_mut();
+    //         let connection = self.connections[curr_layer_i - 1].borrow_mut();
+
+    //         for curr_node_i in 0..curr_layer.len() {
+
+    //             for prev_node_i in 0..prev_layer.len() {
+    //                 // update connetions
+
+    //                 connection.gradient_w[(curr_node_i, prev_node_i)] += curr_layer.error_z[curr_node_i] * prev_layer.value_a[prev_node_i];
+    //             } 
+
+    //             // update bias
+    //             curr_layer.gradient_b[curr_node_i] += curr_layer.error_z[curr_node_i];
+    //         }
+
+    //         // update previous error
+    //         // TODO: Not needed for first layer?
+    //         for prev_node_i in 0..prev_layer.len() {
+    //             let mut total = 0.0;
+
+    //             for curr_node_i in 0..curr_layer.len() {
+    //                 total += curr_layer.error_z[curr_node_i] * connection.value_w[(curr_node_i, prev_node_i)]
+    //             }
+
+    //             prev_layer.error_z[prev_node_i] = total * activate_sigmoid_derivate(prev_layer.unscaled_z[prev_node_i])
+    //         }
+    //     }   
+    // }
+
     fn backpropagete(&mut self, output: &Output) {
-        // set last layer error
         let layers_len = self.layers.len();
-        let last_layer = &mut self.layers[layers_len - 1];
 
-        for i in 0..last_layer.len() {
-            let neuron = &mut last_layer[i];
+        {
+            // set last layer error
+            let last_layer = &mut self.layers[layers_len - 1].borrow_mut();
 
-            // TODO: Why nuron.value_a - output[i] and not vice versa?
-            neuron.error_z =  (neuron.value_a - output[i]) * activate_sigmoid_derivate(neuron.unscaled_z)
+            let mut last_layer_error_z = last_layer.error_z.borrow_mut();
+            let last_layer_value_a = last_layer.value_a.borrow();
+            let last_layer_unscaled_z = last_layer.unscaled_z.borrow();
+
+            last_layer_error_z.iter_mut()
+                .zip(last_layer_value_a.iter())
+                .zip(output.iter())
+                .zip(last_layer_unscaled_z.iter())
+                .for_each(|(((error, value), output), unscaled)| {
+                    *error = (value - output) * activation_function_derivate(*unscaled)
+                });
+
+            // for i in 0..last_layer.len() {
+            //     // TODO: Why nuron.value_a - output[i] and not vice versa?
+            //     last_layer_error_z[i] =  (last_layer_value_a[i] - output[i]) * activate_sigmoid_derivate(last_layer_unscaled_z[i])
+            // }
         }
+
 
         // loop for each previous layer
         for curr_layer_i in (1..layers_len).rev() {
-            for curr_node_i in 0..self.layers[curr_layer_i].len() {
+            let curr_layer = &self.layers[curr_layer_i];
+            let prev_layer = &self.layers[curr_layer_i - 1];
+            let connection = &mut self.connections[curr_layer_i - 1].borrow_mut();
 
-                for prev_node_i in 0..self.layers[curr_layer_i - 1].len() {
+            let curr_error_z = curr_layer.error_z.borrow();
+            let mut prev_error_z = prev_layer.error_z.borrow_mut();
+            let mut curr_gradient_b = curr_layer.gradient_b.borrow_mut();
+            let prev_value_a = prev_layer.value_a.borrow();
+            let prev_unscaled_z = prev_layer.unscaled_z.borrow();
+
+            for curr_node_i in 0..curr_layer.len() {
+                for prev_node_i in 0..prev_layer.len() {
                     // update connetions
-
-                    let prev_node = &self.layers[curr_layer_i - 1][prev_node_i];
-
-                    let connection = &mut self.connections[curr_layer_i - 1][curr_node_i][prev_node_i];
-                    connection.gradient_w += self.layers[curr_layer_i][curr_node_i].error_z * prev_node.value_a;
+                    connection.gradient_w[(curr_node_i, prev_node_i)] += curr_error_z[curr_node_i] * prev_value_a[prev_node_i];
                 } 
-
-                // update bias
-                self.layers[curr_layer_i][curr_node_i].gradient_b += self.layers[curr_layer_i][curr_node_i].error_z;
             }
+
+            // update connetions
+            // connection.gradient_w[(curr_node_i, prev_node_i)] += curr_error_z[curr_node_i] * prev_value_a[prev_node_i];
+            // connection.gradient_w += curr_error_z.t().dot(&*prev_value_a);
+            // let mutli = arr1(xs)
+            // connection.gradient_w.axis_iter_mut(Axis(0)).zip(curr_error_z.iter()).for_each(|(mut gradient, error)| {
+            //     gradient.add_assign(prev_value_a.(*error))
+            // });
+
+            // update bias
+            // curr_gradient_b[curr_node_i] += curr_error_z[curr_node_i];
+            *curr_gradient_b += &*curr_error_z;
+
 
             // update previous error
-            // TODO: Not needed for first layer?
-            for prev_node_i in 0..self.layers[curr_layer_i - 1].len() {
-                let mut total = 0.0;
+            *prev_error_z = connection.value_w.t().dot(&*curr_error_z);
+            prev_error_z.iter_mut().zip(prev_unscaled_z.iter()).for_each(|(err, unscaled)| {
+                *err *= activation_function_derivate(*unscaled);
+            })
 
-                for curr_node_i in 0..self.layers[curr_layer_i].len() {
-                    total += self.layers[curr_layer_i][curr_node_i].error_z * self.connections[curr_layer_i - 1][curr_node_i][prev_node_i].value_w
-                }
-
-                let prev_node = &mut self.layers[curr_layer_i - 1][prev_node_i];
-                prev_node.error_z = total * activate_sigmoid_derivate(prev_node.unscaled_z)
-            }
         }   
     }
 
     fn clear_gradient(&mut self) {
-        for layer in self.layers.iter_mut() {
-            for neuron in layer.iter_mut() {
-                neuron.gradient_b = 0.0;
+        for layer in self.layers.iter() {
+            for neuron in layer.gradient_b.borrow_mut().iter_mut() {
+                *neuron = 0.0;
             }
         }
 
@@ -208,20 +343,21 @@ impl NN {
         for connections in self.connections.iter_mut() {
 
             // Current activation
-            for connections in connections.iter_mut() {
+            for connection in connections.gradient_w.iter_mut() {
 
                 // previous activation
-                for connection in connections.iter_mut() {
-                    connection.gradient_w = 0.0;
-                }
+                *connection = 0.0;
             }
         }
     }
 
     fn apply_gradient(&mut self, rate: f32) {
-        for layer in self.layers.iter_mut() {
-            for neuron in layer.iter_mut() {
-                neuron.bias_b -= neuron.gradient_b * rate;
+        for layer in self.layers.iter() {
+            let mut layer_bias_b = layer.bias_b.borrow_mut();
+            let layer_gradient_b = layer.gradient_b.borrow();
+
+            for i in 0..layer.len() {
+                layer_bias_b[i] -= layer_gradient_b[i] * rate
             }
         }
 
@@ -229,19 +365,24 @@ impl NN {
         for connections in self.connections.iter_mut() {
 
             // Current activation
-            for connections in connections.iter_mut() {
+            // connections.value_w.sub(connections.gradient_w * rate);
 
-                // previous activation
-                for connection in connections.iter_mut() {
-                    connection.value_w -= connection.gradient_w * rate;
-                }
-            }
+            connections.value_w.iter_mut().zip(connections.gradient_w.iter()).for_each(|(value, gradient)| {
+                *value -= gradient * rate
+            })
+
+            // for i in 0..connections.gradient_w.len() {
+            //     // previous activation
+            //     for j in 0..connections.gradient_w[i].len() {
+            //         connections.value_w[(i, j)] -= connections.gradient_w[(i, j)] * rate;
+            //     }
+            // }
         }
     }
 
     pub fn train_samples(&mut self, samples: &[Sample], rate: f32) {
         self.clear_gradient();
-    
+
         samples.iter().for_each(|sample| {
             self.forward(&sample.input);
             self.backpropagete(&sample.output);
@@ -259,9 +400,9 @@ impl NN {
     
         assert_eq!(sample.output.len(), out_layer.len(), "Output layers not of same size!");
     
-    
+        let out_value_a = out_layer.value_a.borrow();
         for i in 0..sample.output.len() {
-            let err = sample.output[i] - out_layer[i].value_a;
+            let err = sample.output[i] - out_value_a[i];
             total += err * err
         };
     
@@ -280,19 +421,42 @@ impl NN {
     }
 }
 
-fn activate_linear(value: f32) -> f32 {
-    if value > 0.0 {
-        value
-    } else {
-        0.0
+enum ActivationFunction {
+    Linear(f32),
+    Sigmoid
+}
+
+// const ACTIVATION_FUNCTION: ActivationFunction = ActivationFunction::Linear(0.0);
+// const ACTIVATION_FUNCTION: ActivationFunction = ActivationFunction::Linear(0.01);
+const ACTIVATION_FUNCTION: ActivationFunction = ActivationFunction::Sigmoid;
+
+fn activation_function(value: f32) -> f32 {
+    match ACTIVATION_FUNCTION {
+        ActivationFunction::Sigmoid => {
+            1.0 / (1.0 + f32::powf(std::f32::consts::E, -value))
+        },
+        ActivationFunction::Linear(a) => {
+            if value > 0.0 {
+                value
+            } else {
+                value * a
+            }
+        }
     }
 }
 
-fn activate_sigmoid(value: f32) -> f32 {
-    1.0 / (1.0 + f32::powf(std::f32::consts::E, -value))
-}
-
-fn activate_sigmoid_derivate(value: f32) -> f32 {
-    let fun = activate_sigmoid(value);
-    fun * (1.0 - fun)
+fn activation_function_derivate(value: f32) -> f32 {
+    match ACTIVATION_FUNCTION {
+        ActivationFunction::Sigmoid => {
+            let fun = activation_function(value);
+            fun * (1.0 - fun)
+        },
+        ActivationFunction::Linear(a) => {
+            if value > 0.0 {
+                1.0
+            } else {
+                a
+            }
+        }
+    }
 }
